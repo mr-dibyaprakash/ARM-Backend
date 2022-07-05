d008d011800d970c6d9a0ea1934fb128c4e578d3
sandeepDe <sunnysan8686@gmail.com>
2022-07-05 12:04:45 +0530

    Base Project Creation

---
+++ src/main/java/com/armapp/AuditRequestManagementApplication.java
@@ -0,0 +1,13 @@
+package com.armapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditRequestManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditRequestManagementApplication.class, args);
+	}
+
+}
