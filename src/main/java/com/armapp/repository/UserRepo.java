4669ef91aca8fa754e1b9316683c1699e28f6110
mr-dibyaprakash <prakash.ojha@acheron-tech.com>
2022-07-10 22:06:03 +0530

    Updated Packages

--- src/main/java/com/armapp/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.armapp.repository;
-
-import com.armapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
