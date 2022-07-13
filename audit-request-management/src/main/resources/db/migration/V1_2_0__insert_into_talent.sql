880c575379f7250f767b9f789cb26be5d3d3c1df
mr-dibyaprakash <prakash.ojha@acheron-tech.com>
2022-07-13 09:36:25 +0530

    Updated Folder Structure

--- audit-request-management/src/main/resources/db/migration/V1_2_0__insert_into_talent.sql
+++ audit-request-management/src/main/resources/db/migration/V1_2_0__insert_into_talent.sql
@@ -1,7 +1,7 @@
 insert into talent(talent_id,talent_name,created_by)values(1,"Dwayne Johnson","Abuthair");
 
 insert into talent(talent_id,talent_name,created_by)values(2,"Alexander Gould","Abuthair");
-
+--
 insert into talent(talent_id,talent_name,created_by)values(3,"Florence Pugh","Abuthair");
 
 insert into talent(talent_id,talent_name,created_by)values(4,"Gemma Chan","Abuthair");
