6144a661642313408b40dd8f57d11c59da278d4b
mr-dibyaprakash <prakash.ojha@acheron-tech.com>
2022-07-14 18:52:36 +0530

    TypeAhead for fields

---
+++ audit-request-management/src/main/java/com/armapp/controller/ProjectController.java
@@ -0,0 +1,46 @@
+package com.armapp.controller;
+
+import com.armapp.model.Project;
+import com.armapp.modelDTOs.ProductionVO;
+import com.armapp.modelDTOs.ProjectVO;
+import com.armapp.service.IProjectService;
+import org.dozer.DozerBeanMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api")
+public class ProjectController {
+
+    IProjectService iProjectService;
+
+    public ProjectController(IProjectService iProjectService) {
+        this.iProjectService = iProjectService;
+    }
+    @GetMapping("/projects/{name}")
+    @RolesAllowed({"admin"})
+    ResponseEntity<List<ProjectVO>> showProjectVODetails(@PathVariable("name") String name){
+        List<Project> projectList = iProjectService.getAllProjectVos(name);
+        List<ProjectVO> projectVOList = new ArrayList<ProjectVO>();
+        DozerBeanMapper mapper = new DozerBeanMapper();
+        List<String> myMappingFiles = new ArrayList<>();
+        myMappingFiles.add("dozerBeanMapping.xml");
+        mapper.setMappingFiles(myMappingFiles);
+        for (Project project : projectList) {
+            ProjectVO projectVO = mapper.map(project, ProjectVO.class);
+            projectVOList.add(projectVO);
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(httpHeaders -> httpHeaders
+                        .add("desc", "get production company names like"))
+                .body(projectVOList);
+    }
+}
+
