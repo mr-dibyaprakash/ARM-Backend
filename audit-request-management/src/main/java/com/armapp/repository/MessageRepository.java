19888954f8339da90fc913fb12d13cbd39e738db
Badal Baidya <badal@Badals-Air.Dlink>
2022-07-17 16:02:07 +0530

    Added code for offline task  messages

---
+++ audit-request-management/src/main/java/com/armapp/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.armapp.repository;
+
+import com.armapp.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Query(value="SELECT m FROM Message m WHERE m.task.taskId = :taskId")
+    List<Message> findAllByTaskId(Integer taskId);
+}
