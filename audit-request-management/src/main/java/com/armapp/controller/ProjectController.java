345a3bc5ad90298cce7be3ed4fbfe891b4fde626
Badal Baidya <badal@Badals-Air.Dlink>
2022-07-22 18:05:27 +0530

    project talent list with contract no

--- audit-request-management/src/main/java/com/armapp/controller/ProjectController.java
+++ audit-request-management/src/main/java/com/armapp/controller/ProjectController.java
@@ -1,8 +1,11 @@
 package com.armapp.controller;
 
 import com.armapp.model.Project;
+import com.armapp.model.Talent;
+import com.armapp.model.TalentProject;
 import com.armapp.service.IProjectService;
 import com.armapp.vo.ProjectVO;
+import com.armapp.vo.TalentVO;
 import org.dozer.DozerBeanMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,13 +31,27 @@ public class ProjectController {
     ResponseEntity<List<ProjectVO>> showTypedProjectNames(@PathVariable("productionId") Integer productionId,@PathVariable("typedProjectName") String typedProjectName){
         List<Project> projectList = iProjectService.getTypedProjectNames(productionId,typedProjectName);
         List<ProjectVO> projectVOList = new ArrayList<ProjectVO>();
-        DozerBeanMapper mapper = new DozerBeanMapper();
-        List<String> myMappingFiles = new ArrayList<>();
-        myMappingFiles.add("dozerBeanMapping.xml");
-        mapper.setMappingFiles(myMappingFiles);
-        for (Project project : projectList) {
-            ProjectVO projectVO = mapper.map(project, ProjectVO.class);
-            projectVOList.add(projectVO);
+        //TODO work on this things
+//        DozerBeanMapper mapper = new DozerBeanMapper();
+//        List<String> myMappingFiles = new ArrayList<>();
+//        myMappingFiles.add("dozerBeanMapping.xml");
+//        mapper.setMappingFiles(myMappingFiles);
+//        for (Project project : projectList) {
+//            ProjectVO projectVO = mapper.map(project, ProjectVO.class);
+//            projectVOList.add(projectVO);
+//        }
+
+        for(Project project:  projectList){
+            ProjectVO projectVO = new ProjectVO();
+            projectVO.setProjectName(project.getProjectName());
+            List<TalentVO> talentVOList =  new ArrayList<>();
+            for(TalentProject talentProject: project.getTalentProjects()){
+                TalentVO talentVO = new TalentVO();
+                talentVO.setTalentName(talentProject.getTalent().getTalentName());
+                talentVO.setContractNo(talentProject.getContractNumber());
+                talentVOList.add(talentVO);
+            }
+            projectVO.setTalentVOList(talentVOList);
         }
         return ResponseEntity.status(HttpStatus.OK)
                 .headers(httpHeaders -> httpHeaders
--- audit-request-management/src/main/java/com/armapp/vo/TalentVO.java
+++ audit-request-management/src/main/java/com/armapp/vo/TalentVO.java
@@ -10,6 +10,6 @@ import lombok.*;
 public class TalentVO {
 
     private String talentName;
-
+    private String contractNo;
 
 }
